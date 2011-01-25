4180728ec04a4a4f166dc5d0b6cdbc0c05d43700
wwqgtxx <wwqgtxx@edbefd46-5c8b-4efb-88af-2ab01ea2870e>
2011-01-25 13:06:39 +0000

    git-svn-id: svn://svn.code.sf.net/p/wlogging/code@2 edbefd46-5c8b-4efb-88af-2ab01ea2870e

---
+++ WLogging/src/net/sf/wlogging/Paint.java
@@ -0,0 +1,11 @@
+package net.sf.wlogging;
+
+public class Paint {
+	public void p(Object o) {
+		System.out.println(o); 
+	}
+	
+	public static void paint(Object o) {
+		System.out.println(o); 
+	}
+}
